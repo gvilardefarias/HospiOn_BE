92feb50a6bf1c176e37d7306cd578a1602258641
Gustavo Vilar de Farias <gustavo.farias@ee.ufcg.edu.br>
2020-04-18 19:04:23 -0300

    First version of auth service

---
+++ methods.py
@@ -0,0 +1,14 @@
+import os.path
+
+def userRegistered(email):
+    return os.path.exists('users/' + email + '.json')
+
+def register(name, surname, email, tipo):
+    data = {'name': name, 'surname': surname, 'email': email, 'type': tipo}
+
+    arquivo = open('users/' + email + '.json', 'w')
+
+    arquivo.write(json.dumps(data))
+
+    arquivo.close()
+
--- services.py
+++ services.py
@@ -1,21 +1,62 @@
 import requests
+import jwt
+import json
 import os
 from flask import Flask, jsonify, request
+import methods
 
 app = Flask(__name__)
+
+secretWorld = "biiirHospOn"
 
 @app.route('/')
 def hello_world():
     return jsonify({"message": "Hello Heroku"})
 
-@app.route('/auth')
-def authorization():
+@app.route('/auth', methods=['GET'])
+def authorizationGoogle():
     auth     = request.authorization
     authType = request.args.get('by')
 
-   # if authType=='google':
-    return requests.get('https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=asdas').text
+    if authType=='google':
+        r    = requests.get('https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=' + str(auth))
+        data = r.json()
 
+        if r.status_code==200:
+            email = data['email']
+
+            data['token'] = jwt.encode({'email': email}, secretWorld, algorithm='HS256')
+
+            if not methods.userRegistered(email):
+                methods.register(data['given_name'], data['family_name'], email, "PF")
+    
+            return jsonify(data)
+
+
+    return jsonify({"message": "Authentication Failed"}), 400
+
+@app.route('/auth', methods=['POST'])
+def authorizationFacebook():
+    auth     = request.authorization
+    authType = request.args.get('by')
+
+    content  = request.json
+
+    if authType=='facebook':
+        data = r.json()
+
+        if r.status_code==200:
+            r    = requests.get('https://graph.facebook.com/me?access_token=' + str(auth))
+            email = content['email']
+
+            data['token'] = jwt.encode({'email': email}, secretWorld, algorithm='HS256')
+
+            if not methods.userRegistered(email):
+                methods.register(data['name'], '', email, "PF")
+    
+            return jsonify(data)
+
+    return jsonify({"message": "Authentication Failed"}), 400
     
 
 if __name__ == '__main__':
