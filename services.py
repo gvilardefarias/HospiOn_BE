8d9bb78851e24ef605385a77079cfd061a26b280
Gustavo Vilar de Farias <gustavo.farias@ee.ufcg.edu.br>
2020-04-18 15:44:48 -0300

    Initial authorization method

--- services.py
+++ services.py
@@ -7,6 +7,16 @@ app = Flask(__name__)
 def hello_world():
     return jsonify({"message": "Hello Heroku"})
 
+@app.route('/auth')
+def authorization():
+    auth     = request.authorization
+    authType = request.args.get('by')
+
+   # if authType=='google':
+    return request.get('https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=asdas')
+
+    
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
