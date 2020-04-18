9d9fe335c71e4c005668e2cc0170a8811b5bbbf3
Gustavo Vilar de Farias <gustavo.farias@ee.ufcg.edu.br>
2020-04-18 10:57:59 -0300

    Build to heroku

--- services.py
+++ services.py
@@ -1,10 +1,10 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return jsonify({"message": "Hello Heroku"})
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
