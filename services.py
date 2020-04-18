674d3592796cc10ddcedec271b35bb20975d2a5a
Gustavo Vilar de Farias <gustavo.farias@ee.ufcg.edu.br>
2020-04-18 10:42:14 -0300

    Build to heroku

--- services.py
+++ services.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
@@ -6,4 +7,5 @@ def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port='5000', debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
